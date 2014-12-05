8ed3fa4516a1a5788c8413b633686738766f57dd
xu_cl <xu_cl@www.googleadservices.com>
2014-12-05 16:31:56 +0800

    eidt

--- XCLJsTool/Web/Js/XCLJsTool.js
+++ XCLJsTool/Web/Js/XCLJsTool.js
@@ -1118,6 +1118,17 @@
          */
         Range:function(min,max){
             return Math.random()*(max-min)+min;
+        },
+        /**
+         * 生成guid（此guid可能会重复，如果是关键的地方，请勿使用）
+         * @returns {String} 所生成的guid
+         */
+        Guid:function(){
+            var S4=function() {
+                return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+            };
+            var guid = (S4() + S4() + "-" + S4() + S4() + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+            return guid;  
         }
     };
     
